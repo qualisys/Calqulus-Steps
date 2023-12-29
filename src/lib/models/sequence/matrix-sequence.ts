9211baa45cde42389f678bce732d301d43d920e9
Vincent Fohanno <vincent.fohanno@qualisys.se>
2023-12-29 16:44:56 +0100

    Add new methods in MatrixSequence #32228

--- src/lib/models/sequence/matrix-sequence.ts
+++ src/lib/models/sequence/matrix-sequence.ts
@@ -1,5 +1,6 @@
 import { Matrix } from '../spatial/matrix';
 
+import { QuaternionSequence } from './quaternion-sequence';
 import { VectorSequence } from './vector-sequence';
 
 export class MatrixSequence {
@@ -47,6 +48,43 @@ export class MatrixSequence {
 			new Float32Array(nan), new Float32Array(nan), new Float32Array(nan), new Float32Array(nan),
 			new Float32Array(nan), new Float32Array(nan), new Float32Array(nan), new Float32Array(nan),
 			new Float32Array(nan), new Float32Array(nan), new Float32Array(nan), new Float32Array(nan)
+		);
+	}
+
+	/**
+	 * Create an Identity Matrix sequence (diagonal filed with 1) for the specified
+	 * length .
+	 *
+	 * @param length the length of the MatrixSequence
+	 * @returns An Identity MatrixSequence.
+	 */
+	static createIdentity(length: number) {
+		const ones = new Float32Array(length).fill(1);
+		const zeroes = new Float32Array(length).fill(0);
+
+		return new MatrixSequence(
+			new Float32Array(ones), new Float32Array(zeroes), new Float32Array(zeroes), new Float32Array(zeroes),
+			new Float32Array(zeroes), new Float32Array(ones), new Float32Array(zeroes), new Float32Array(zeroes),
+			new Float32Array(zeroes), new Float32Array(zeroes), new Float32Array(ones), new Float32Array(zeroes),
+			new Float32Array(zeroes), new Float32Array(zeroes), new Float32Array(zeroes), new Float32Array(ones)
+		);
+	}
+
+	/**
+	 * Create a Matrix sequence filled with 0 for the specified
+	 * length .
+	 *
+	 * @param length the length of the MatrixSequence
+	 * @returns An MatrixSequence filled with 0.
+	 */
+	static createZero(length: number) {
+		const zeroes = new Float32Array(length).fill(0);
+
+		return new MatrixSequence(
+			new Float32Array(zeroes), new Float32Array(zeroes), new Float32Array(zeroes), new Float32Array(zeroes),
+			new Float32Array(zeroes), new Float32Array(zeroes), new Float32Array(zeroes), new Float32Array(zeroes),
+			new Float32Array(zeroes), new Float32Array(zeroes), new Float32Array(zeroes), new Float32Array(zeroes),
+			new Float32Array(zeroes), new Float32Array(zeroes), new Float32Array(zeroes), new Float32Array(zeroes)
 		);
 	}
 
@@ -117,6 +155,40 @@ export class MatrixSequence {
 	}
 
 	/**
+	 * Create a MatrixSequence for the specified length.
+	 * 
+	 * @param Matrix
+	 * @param length
+	 * @returns A MatrixSequence with the specified length.
+	 */
+	static fromMatrix(matrix: Matrix, length: number) {
+
+		const m00Array = new Float32Array(length).fill(matrix._m[0]);
+		const m01Array = new Float32Array(length).fill(matrix._m[1]);
+		const m02Array = new Float32Array(length).fill(matrix._m[2]);
+		const m03Array = new Float32Array(length).fill(matrix._m[3]);
+		const m10Array = new Float32Array(length).fill(matrix._m[4]);
+		const m11Array = new Float32Array(length).fill(matrix._m[5]);
+		const m12Array = new Float32Array(length).fill(matrix._m[6]);
+		const m13Array = new Float32Array(length).fill(matrix._m[7]);
+		const m20Array = new Float32Array(length).fill(matrix._m[8]);
+		const m21Array = new Float32Array(length).fill(matrix._m[9]);
+		const m22Array = new Float32Array(length).fill(matrix._m[10]);
+		const m23Array = new Float32Array(length).fill(matrix._m[11]);
+		const m30Array = new Float32Array(length).fill(matrix._m[12]);
+		const m31Array = new Float32Array(length).fill(matrix._m[13]);
+		const m32Array = new Float32Array(length).fill(matrix._m[14]);
+		const m33Array = new Float32Array(length).fill(matrix._m[15]);
+
+		return new MatrixSequence(
+			m00Array, m01Array, m02Array, m03Array,
+			m10Array, m11Array, m12Array, m13Array,
+			m20Array, m21Array, m22Array, m23Array,
+			m30Array, m31Array, m32Array, m33Array,
+		);
+	}
+
+	/**
 	 * Create a MatrixSequence from the specified arrays.
 	 * 
 	 * @param m00 Component in column 0, row 0 position (index 0)
@@ -145,6 +217,23 @@ export class MatrixSequence {
 			new Float32Array(zero), new Float32Array(zero), new Float32Array(zero), new Float32Array(len).fill(1),
 		);
 	}
+
+	/**
+	 * Calculates a 4x4 matrix sequence from the given quaternion sequence.
+	 *
+	 * @param q Quaternion sequence to create matrix from.
+	 * @param result The Matrix instance to store the result in.
+	 *
+	 * @returns The resulting matrix sequence.
+	 */
+	static fromQuaternionSequence(q: QuaternionSequence): MatrixSequence {
+		const result = MatrixSequence.createEmpty(q.length);		
+		for (let i = 0; i < q.length; i++) {
+			const m = Matrix.fromQuaternion(q.getQuaternionAtFrame(i + 1), Matrix.tmpMat1);
+			result.setMatrixAtFrame(i + 1, m);
+		}
+		return result; 
+	}	
 
 	/**
 	 * The number of elements in this sequence.
@@ -185,6 +274,60 @@ export class MatrixSequence {
 		m[15] = this.m33[frameIndex];
 
 		return matrix;
+	}
+
+	/**
+	 * Multiplies this matrix sequence with another matrix sequence.
+	 * 
+	 * @param otherMatrix The sequence to multiply with.
+	 * @param result The sequence to store the result in.
+	 * @returns The resulting matrix sequence.
+	 */
+	multiply(otherMatrix: MatrixSequence, result?: MatrixSequence): MatrixSequence {
+		const len = Math.max(this.length, otherMatrix.length);
+		const m00 = result ? result.m00 : new Float32Array(len);
+		const m01 = result ? result.m01 : new Float32Array(len);
+		const m02 = result ? result.m02 : new Float32Array(len);
+		const m03 = result ? result.m03 : new Float32Array(len);
+		const m10 = result ? result.m00 : new Float32Array(len);
+		const m11 = result ? result.m11 : new Float32Array(len);
+		const m12 = result ? result.m12 : new Float32Array(len);
+		const m13 = result ? result.m13 : new Float32Array(len);
+		const m20 = result ? result.m20 : new Float32Array(len);
+		const m21 = result ? result.m21 : new Float32Array(len);
+		const m22 = result ? result.m22 : new Float32Array(len);
+		const m23 = result ? result.m23 : new Float32Array(len);
+		const m30 = result ? result.m30 : new Float32Array(len);
+		const m31 = result ? result.m31 : new Float32Array(len);
+		const m32 = result ? result.m32 : new Float32Array(len);
+		const m33 = result ? result.m33 : new Float32Array(len);
+
+		for (let i = 0; i < len; i++) {
+			const i0 = Math.min(i, this.length - 1);
+			const i1 = Math.min(i, otherMatrix.length - 1);
+			const matrix0 = this.getMatrixAtFrame(i0 + 1);
+			const matrix1 = otherMatrix.getMatrixAtFrame(i1 + 1);
+			const r = matrix0.multiply(matrix1);
+
+			m00[i] = r._m[0];
+			m01[i] = r._m[1];
+			m02[i] = r._m[2];
+			m03[i] = r._m[3];
+			m10[i] = r._m[4];
+			m11[i] = r._m[5];
+			m12[i] = r._m[6];
+			m13[i] = r._m[7];
+			m20[i] = r._m[8];
+			m21[i] = r._m[9];
+			m22[i] = r._m[10];
+			m23[i] = r._m[11];
+			m30[i] = r._m[12];
+			m31[i] = r._m[13];
+			m32[i] = r._m[14];
+			m33[i] = r._m[15];
+		}
+
+		return result ? result : new MatrixSequence(m00, m01, m02, m03, m10, m11, m12, m13, m20, m21, m22, m23, m30, m31, m32, m33);
 	}
 
 	/** 
