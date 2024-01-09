7e204e55f95c5cd519ecc5ff2e121f5ea267f050
Vincent Fohanno <vincent.fohanno@qualisys.se>
2024-01-09 18:36:21 +0100

    Fix missing scalar multiplication in method #32228

--- lib/models/sequence/matrix-sequence.ts
+++ lib/models/sequence/matrix-sequence.ts
@@ -385,22 +385,22 @@ export class MatrixSequence {
 		const m33 = result ? result.m33 : new Float32Array(len);
 		
 		for (let i = 0; i < len; i++) {
-			m00[i] = this.m00[0];
-			m01[i] = this.m01[1];
-			m02[i] = this.m02[2];
-			m03[i] = this.m03[3];
-			m10[i] = this.m10[4];
-			m11[i] = this.m11[5];
-			m12[i] = this.m12[6];
-			m13[i] = this.m13[7];
-			m20[i] = this.m20[8];
-			m21[i] = this.m21[9];
-			m22[i] = this.m22[10];
-			m23[i] = this.m23[11];
-			m30[i] = this.m30[12];
-			m31[i] = this.m31[13];
-			m32[i] = this.m32[14];
-			m33[i] = this.m33[15];
+			m00[i] = this.m00[i] * n;
+			m01[i] = this.m01[i] * n;
+			m02[i] = this.m02[i] * n;
+			m03[i] = this.m03[i] * n;
+			m10[i] = this.m10[i] * n;
+			m11[i] = this.m11[i] * n;
+			m12[i] = this.m12[i] * n;
+			m13[i] = this.m13[i] * n;
+			m20[i] = this.m20[i] * n;
+			m21[i] = this.m21[i] * n;
+			m22[i] = this.m22[i] * n;
+			m23[i] = this.m23[i] * n;
+			m30[i] = this.m30[i] * n;
+			m31[i] = this.m31[i] * n;
+			m32[i] = this.m32[i] * n;
+			m33[i] = this.m33[i] * n;
 		}
 
 		return result ? result : new MatrixSequence(m00, m01, m02, m03, m10, m11, m12, m13, m20, m21, m22, m23, m30, m31, m32, m33);
