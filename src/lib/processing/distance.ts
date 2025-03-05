5c85c1e0089df53cd28acf1ddb65d77cfa238a1f
Martin Hansson <martin.hansson@qualisys.se>
2025-03-05 16:17:51 +0100

    Support single sequence in distance step. #36810

--- src/lib/processing/distance.ts
+++ src/lib/processing/distance.ts
@@ -13,49 +13,75 @@ import { BaseStep } from './base-step';
 	category: 'Geometry',
 	description: markdownFmt`
 		Accepts marker or segment sequences and calculates the distance 
-		between the points (Euclidean norm).
-
-		It assumes that the values are comparable by index. If the sequence 
+		between the points (Euclidean norm). 
+		
+		If two sequences are provided, the calculation is done between the 
+		corresponding points of the sequences. If the sequence 
 		length differs, the calculation will be done up until the shortest 
-		length of the input sequences.`,
+		length of the input sequences.
+
+		If one sequence is provided, the calculation is done between the points
+		of the sequence.
+
+		It assumes that the values are comparable by index. 
+		The output is a series of distances between the points.`,
 	inputs: [
 		{ type: ['Scalar', 'Series (<vector> | <segment>)'] },
-		{ type: ['Scalar', 'Series (<vector> | <segment>)'] },
+		{ type: ['Scalar', 'Series (<vector> | <segment>)'], optional: true },
 	],
 	output: ['Scalar', 'Series'],
 })
 export class DistanceStep extends BaseStep {
 	async process(): Promise<Signal> {
-		if (this.inputs.length < 2) {
-			throw new ProcessingError('Two input signals required.');
+		let dist: Float32Array;
+
+		if (this.inputs.length < 1 || this.inputs.length > 2) {
+			throw new ProcessingError('One or two input signals required.');
 		}
 
-		for (const input of this.inputs.slice(0, 2)) {
+		for (const input of this.inputs) {
 			if (![SignalType.Segment, SignalType.VectorSequence].includes(input.type)) {
 				throw new ProcessingError(`Unexpected type. Expected Segment or Vector, got ${ input.typeToString }.`);
 			}
 		}
 
 		const a = this.inputs[0].getValue() as Segment | VectorSequence;
-		const b = this.inputs[1].getValue() as Segment | VectorSequence;
-
 		const ax = a.getComponent('x');
 		const ay = a.getComponent('y');
 		const az = a.getComponent('z');
+		
+		if (this.inputs.length === 2) {
+			const b = this.inputs[1].getValue() as Segment | VectorSequence;
+			const bx = b.getComponent('x');
+			const by = b.getComponent('y');
+			const bz = b.getComponent('z');
+			
+			const d = new Vector(0, 0, 0);
+			const length = Math.min(a.length, b.length);
+			dist = new Float32Array(length);
+			
+			for (let i = 0; i < length; i++) {
+				d.x = bx[i] - ax[i];
+				d.y = by[i] - ay[i];
+				d.z = bz[i] - az[i];
+				dist[i] = Vector.norm(d);
+			}
+		}
+		else {
+			const length = a.length;
+			if (length < 2) {
+				throw new ProcessingError('At least two points are required to calculate distances.');
+			}
 
-		const bx = b.getComponent('x');
-		const by = b.getComponent('y');
-		const bz = b.getComponent('z');
+			const d = new Vector(0, 0, 0);
+			dist = new Float32Array(length - 1);
 
-		const length = Math.min(a.length, b.length);
-		const dist = new Float32Array(length);
-		const d = new Vector(0, 0, 0);
-
-		for (let i = 0; i < length; i++) {
-			d.x = bx[i] - ax[i];
-			d.y = by[i] - ay[i];
-			d.z = bz[i] - az[i];
-			dist[i] = Vector.norm(d);
+			for (let i = 1; i < length; i++) {
+				d.x = ax[i] - ax[i - 1];
+				d.y = ay[i] - ay[i - 1];
+				d.z = az[i] - az[i - 1];
+				dist[i - 1] = Vector.norm(d);
+			}
 		}
 
 		return this.inputs[0].clone(dist);
